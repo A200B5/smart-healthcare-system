e822c2c95cfa3067fc0afccd3768e34120c2afd0
AhmedBakr200 <20250907@stud.fci-cu.edu.eg>
2026-06-21 19:47:44 +0300

    remove an unsed-files

--- frontend/src/pages/Dashboard.jsx
+++
@@ -1,5 +0,0 @@
-function Landing() {
-  return <h2>Dashboard Page</h2>
-}
-
-export default Landing
--- frontend/src/pages/Register.jsx
+++
@@ -1,5 +0,0 @@
-function Landing() {
-  return <h2>Register Page</h2>
-}
-
-export default Landing
