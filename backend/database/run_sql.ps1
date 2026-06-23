3ba17bcbf4c29284ad3a1bc90ee04f758f3c416a
Ahmed Bakr <ab163545@gmail.com>
2026-06-23 03:43:22 +0300

    Delete backend/database directory

--- backend/database/run_sql.ps1
+++
@@ -1,7 +0,0 @@
-$sqlConn = New-Object System.Data.SqlClient.SqlConnection("Server=localhost\MSSQLSERVER01;Database=depi;User Id=sa;Password=123456;TrustServerCertificate=True;")
-$sqlConn.Open()
-$cmd = $sqlConn.CreateCommand()
-$cmd.CommandText = Get-Content -Path "c:\Users\ab163\OneDrive\Documenti\depi-project\depi-project\backend\database\alter_view.sql" -Raw
-$cmd.ExecuteNonQuery()
-$sqlConn.Close()
-Write-Host "Success"
